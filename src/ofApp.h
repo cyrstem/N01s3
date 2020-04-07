95d6d5c4096326825e0a3bdc3faf6a06d6b3af9c
jacos <cyrstem@gmail.com>
2020-04-07 15:49:06 -0500

    create visual module complete start interaction with sound and particle elements

--- src/ofApp.h
+++ src/ofApp.h
@@ -1,47 +1,24 @@
 #pragma once
 
 #include "ofMain.h"
-#include "Particle.h"
+#include "Visual/VisualSystem.h"
+//#include "Particle.h"
 #include "UI/UI.h"
 
 class ofApp : public ofBaseApp{
 
 	public:
 		void setup();
-		void env();
 		void update();
-		void scene();
 		void draw();
 		void keyPressed(int key);
-		void drawCorner(ofPoint p);
 		//send time
 		float time;
-		//sphere 
-		ofSpherePrimitive sphere;
-		// remplacement of this  ofSpherePrimitive sphere;
-		// with a mesh
 
-		ofMesh mesh;
-
-		//cam
-		ofEasyCam cam;
-		//ofCamera cam;
-		glm::vec3 centro;
-		ofFbo fbo;
-		
-		//particle  call  should improve  later;
-		vector<Particle> p;
-		bool dead;
-		//save vertices  // maybe not needed here 
-		vector<glm::vec3> vertices;
-		//shaders
-
-
-		//luz stafff
-		ofLight ambient ,areaLight ,spot;
-		ofMaterial material;
-	public:
+	private:
 		UI ui;
+
+		VisualSystem v;
 	
 
 };
